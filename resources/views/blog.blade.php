7463ff1773982d3411924b0fd3f23a6538d9d346
Raden Adhyaksa <raksa69@gmail.com>
2018-09-07 21:10:31 +0700

    create content blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,1 +1,23 @@
-<h1>Selamat datang di halaman blog...</h1>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Blog</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
